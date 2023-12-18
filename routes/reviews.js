d598c1d158f032ede86dbc0b73faa4f214abb4ec
Pratik8102 <leetcodesharma@gmail.com>
2023-12-18 09:16:04 +0530

    session537

--- routes/reviews.js
+++ routes/reviews.js
@@ -20,7 +20,7 @@ router.post('/',isLoggedIn, validateReview, catchAsync(async(req,res)=>{
     res.redirect(`/campgrounds/${campground._id}`);
   }))
   
-  router.delete('/:reviewId',catchAsync(async(req,res)=>{
+  router.delete('/:reviewId',isLoggedIn,catchAsync(async(req,res)=>{
     const {id,reviewId}=req.params;
     await Campground.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
     await Review.findByIdAndDelete(reviewId);
