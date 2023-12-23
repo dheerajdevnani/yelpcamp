d594fae37fd4a2e37b6a721b2327253f2fc812fb
Pratik8102 <leetcodesharma@gmail.com>
2023-12-23 18:07:15 +0530

    562

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,9 +1,21 @@
 
-    mapboxgl.accessToken=mapToken;
-    const map=new mapboxgl.Map({
-       container: 'map',
-       style:'mapbox://styles/mapbox/streets-v11',
-       center:[-74.5,40],
-       zoom:4
-    });
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: [-74.5,40], // starting position [lng, lat]
+    zoom: 6, // starting zoom
+});
 
+const nav = new mapboxgl.NavigationControl();
+map.addControl(nav, 'top-right');
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({offset:25})
+           .setHTML(
+            `<h3>${campground.title}</h3> <p>${campground.location}</P>`
+            )
+    )
+    .addTo(map)
